3531af6ad4611be6e61c562634ec78f05e6c8e6e
Keith Charles <kcharles1972@gmail.com>
2022-12-27 16:35:43 -0700

    add classes to icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,6 +1,7 @@
 <script>
   export let name;
   export let label = '';
+  export let classNames = '';
 </script>
 
-<i class={name} aria-label={label} />
+<i class={`${name} ${classNames}`} aria-label={label} />
