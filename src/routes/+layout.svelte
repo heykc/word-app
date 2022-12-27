6e126acef97593278152d4ff6d4fc6d701af0421
Keith Charles <kcharles1972@gmail.com>
2022-12-27 14:47:44 -0700

    add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
   import '../app.css';
+  import Icon from '$lib/Icon.svelte';
 </script>
+
+<header class="w-100 bg-zinc-900 p-5">
+  <nav class="w-full">
+    <div class="flex flex-wrap justify-between">
+      <a href="/" class="font-semibold">What's the word?</a>
+      <a href="/about"><Icon name="fa-solid fa-circle-question" label="about" /></a>
+    </div>
+  </nav>
+</header>
 
 <slot />
