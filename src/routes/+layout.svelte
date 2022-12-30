1d0d93c61c9640065557a333314ea4d249352030
Keith Charles <kcharles1972@gmail.com>
2022-12-30 01:30:41 -0700

    remove unused links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,8 @@
 <header class="w-100 p-5">
   <nav class="w-full">
     <div class="flex flex-wrap justify-between">
-      <a href="/" class="font-semibold">What's the word?</a>
-      <a href="/about" aria-label="about page"><Icon name="fa-solid fa-circle-question" /></a>
+      <div class="font-semibold">What's the word?</div>
+      <!-- <a href="/about" aria-label="about page"><Icon name="fa-solid fa-circle-question" /></a> -->
     </div>
   </nav>
 </header>
