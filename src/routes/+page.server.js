0142fe7f61d417d6f39f03f7318677bdd9506981
Keith Charles <kcharles1972@gmail.com>
2022-12-29 23:30:21 -0700

    refactor payload format

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,4 @@
 import {
-  RANDOM_WORD_URL,
-  RANDOM_WORD_HOST,
   RANDOM_WORD_KEY,
   THESAURUS_KEY,
   THESAURUS_URL,
@@ -19,45 +17,37 @@ dayjs.extend(timezone);
 
 const tz = 'America/New_York';
 
+const formatString = (str) => str.replace(/\W/g, '').toLowerCase().trim();
+
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ fetch }) {
-  // const { result } = await getCache(fetch);
-  // const cache = result && JSON.parse(result);
-  // const dayjsLocal = dayjs();
-  // const dayjsNY = dayjsLocal.tz(tz);
+  const { result } = await getCache(fetch);
+  const cache = result && JSON.parse(result);
+  const dayjsLocal = dayjs();
+  const dayjsNY = dayjsLocal.tz(tz);
 
-  // if (cache && !dayjsNY.isAfter(dayjs(cache.createdAt), 'day')) {
-  //   return {
-  //     status: 200,
-  //     body: { selectedWord: cache.selectedWord }
-  //   };
-  // }
+  if (cache && !dayjsNY.isAfter(dayjs(cache.createdAt), 'day')) {
+    return {
+      status: 200,
+      body: { selectedWord: cache.selectedWord }
+    };
+  }
 
-  // const randWord = await getRandomWord(fetch);
-
-  // const randWord = await getWord(fetch);
+  const randWord = await getWord(fetch);
 
   // sometimes thesaurus api doesn't have an entry for the word
   // so getSynonyms will recursively call itself until it finds a word
-  // const selectedWord = await getSynonyms(fetch, 'cynical');
+  const selectedWord = await getSynonyms(fetch, randWord.word);
+  
+  const frequencies = await getFrequencies(fetch, selectedWord.words);
+  frequencies.forEach(({ word, score }) => {
+    selectedWord.words[formatString(word)].score = score;
+  });
 
-  // selectedWord.synonyms = await addFrequencies(fetch, selectedWord.synonyms);
-
-  const selectedWord = {
-    word: 'cynical',
-    definition: 'believing that people are motivated by self-interest; distrustful of human sincerity or integrity',
-    synonyms: [
-      { word: 'pesimistic', frequency: '4.9' },
-      { word: 'misanthropic', frequency: '1.3' },
-      { word: 'cynical', frequency: '3.2' },
-    ],
-    wordType: 'adjective',
-  };
-
-  // await setCache(fetch, {
-  //   selectedWord,
-  //   createdAt: dayjsNY.toISOString(),
-  // });
+  await setCache(fetch, {
+    selectedWord,
+    createdAt: dayjsNY.toISOString(),
+  });
 
   return {
     status: 200,
@@ -76,59 +66,46 @@ const getWord = async (fetch, searchWord = '') => {
   });
 
   if (res.ok) {
-    const { word, frequency = '0' } = await res.json();
-    return { word, frequency };
+    const { word, frequency = '2' } = await res.json();
+    return { word, score: (7 - +frequency).toFixed(1) };
   }
 
   if (res.status === 404) {
-    return { searchWord, frequency: '0' };
+    return { word: searchWord, score: '5.0' };
   }
 
   throw error(500, 'Failed to fetch random word');
 };
 
-const getRandomWord = async (fetch) => {
-  const res = await fetch(RANDOM_WORD_URL, {
-    headers: {
-      'X-RapidAPI-Key': RANDOM_WORD_KEY,
-      'X-RapidAPI-Host': RANDOM_WORD_HOST
-    },
-  });
-
-  if (res.ok) {
-    const word = await res.text();
-    return word;
-  }
-
-  throw error(500, 'Failed to fetch random word');
-}
-
-const addFrequencies = async (fetch, synonyms) => {
-  const synonymsWithFrequencies = await Promise.all(synonyms.map((word) => {
+const getFrequencies = async (fetch, words) => {
+  const wordsWithFrequencies = await Promise.all(Object.values(words).map(({ word }) => {
     return getWord(fetch, word);
   }));
 
-  console.log(synonymsWithFrequencies);
-
-  return synonymsWithFrequencies;
+  return wordsWithFrequencies;
 };
 
 const parseSynonyms = ({ meta: { id: word, syns }, fl, sls, shortdef: definitions }) => {
   const index = Math.floor(Math.random() * syns.length);
   const definition = definitions[index];
-  const synonyms = syns[index].filter((s) => !definition.includes(s));
+  const words = syns[index]
+    .filter((s) => !definition.includes(s))
+    .reduce((acc, cur) => {
+      acc[formatString(cur)] = { word: cur, score: 0 };
+      return acc;
+    }, {});
   let wordType = fl;
 
-  synonyms.push(word);
+  words[formatString(word)] = { word, score: 0 };
 
   if (wordType === 'noun' && sls?.some((s) => s.includes('plural'))) {
     wordType = 'plural noun';
   }
 
   return {
-    word,
+    id: word,
     definition,
-    synonyms,
+    words,
     wordType,
   }
 };
