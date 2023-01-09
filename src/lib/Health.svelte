38a9b02bec03a1b10e4a05961a2487785d0d0293
Keith Charles <kcharles1972@gmail.com>
2023-01-08 17:51:59 -0700

    animate heart icons

--- src/lib/Health.svelte
+++ src/lib/Health.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fly } from 'svelte/transition';
   import Icon from '$lib/Icon.svelte';
 
   export let health;
@@ -10,7 +11,15 @@
   {#each healthArr as i}
     {@const name = i < health ? 'fa-solid fa-heart' : 'fa-regular fa-heart'}
     {@const classNames = i < health ? 'text-red-400' : 'text-zinc-400'}
-
-    <Icon {name} {classNames} />
+      <span class="relative min-w-[20px]">
+        {#if i < health}
+          <span out:fly={{ y: -20, duration: 400 }} class="block relative z-10">
+            <Icon name="fa-solid fa-heart" classNames="text-red-400" />
+          </span>
+        {/if}
+        <span class="absolute top-0 left-0 w-full h-full">
+          <Icon name="fa-regular fa-heart" classNames="text-zinc-400" />
+        </span>
+      </span>
   {/each}
 </div>
