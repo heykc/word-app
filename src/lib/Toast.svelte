0c8f0f0690ff63b9dfc3a8c5085a6c0000237055
Keith Charles <kcharles1972@gmail.com>
2023-01-27 18:13:21 -0700

    use match func to set bgColor based on toast type

--- src/lib/Toast.svelte
+++ src/lib/Toast.svelte
@@ -3,11 +3,19 @@
   import { toasts, removeToast } from '$lib/stores/toast.js';
   import { onMount } from 'svelte';
   import Icon from '$lib/Icon.svelte';
+  import { match } from '$lib/utils.js';
 
   export let message = '';
   export let type = 'info';
   export let delay = 2000;
   export let id;
+
+  $: bgColor = match(type, [
+    ['success', 'bg-green-700'],
+    ['warning', 'bg-amber-700'],
+    ['error', 'bg-red-700'],
+    ['_', 'bg-cyan-700'],
+  ]);
 
   onMount(() => {
     let thisId = id;
@@ -23,7 +31,7 @@
 <div
   in:fly={{ y: -100, duration: 400 }}
   out:fade={{ duration: 200 }}
-  class="toast rounded-md p-3 text-zinc-50 bg-cyan-700 border-cyan-800/40 border-2 w-full relative"
+  class="toast rounded-md p-3 text-zinc-50 {bgColor} border-zinc-50 border-2 w-full relative"
 >
   <button
     class="absolute top-0 right-0 w-6 h-6 rounded-full "
