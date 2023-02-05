bfcff96c6d513ceb61dd2a7efd985242bcdce44d
Keith Charles <kcharles1972@gmail.com>
2023-02-05 10:04:01 -0700

    replace Icon with NpIcon in Health

--- src/lib/Health.svelte
+++ src/lib/Health.svelte
@@ -1,6 +1,6 @@
 <script>
   import { fly } from 'svelte/transition';
-  import Icon from '$lib/Icon.svelte';
+  import NpIcon from '$lib/NpIcon.svelte';
 
   export let health;
   export let totalHealth
@@ -10,17 +10,24 @@
 
 <div class="flex justify-start gap-2 text-xl">
   {#each healthArr as i}
-    {@const name = i < health ? 'fa-solid fa-heart' : 'fa-regular fa-heart'}
-    {@const classNames = i < health ? 'text-red-400' : 'text-zinc-400'}
-      <span class="relative min-w-[20px]">
+      <span class="relative min-w-[24px]">
         {#if i < health}
           <span out:fly={{ y: -20, duration: 400 }} class="block relative z-10">
-            <Icon name="fa-solid fa-heart" classNames="text-red-400" />
+            <NpIcon name="heart" classNames="text-red-400" />
+          </span>
+        {:else}
+          <span class="absolute top-0 left-0 w-full h-full">
+            <NpIcon name="heart" classNames="text-zinc-400 empty-heart" />
           </span>
         {/if}
-        <span class="absolute top-0 left-0 w-full h-full">
-          <Icon name="fa-regular fa-heart" classNames="text-zinc-400" />
-        </span>
       </span>
   {/each}
 </div>
+
+<style>
+  :global(.empty-heart) {
+    stroke: currentColor;
+    stroke-width: 0.2em;
+    fill: transparent !important;
+  }
+</style>
