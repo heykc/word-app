24c8ee6cb06bf4f3608a91e97acaf76c6983f6bf
Keith Charles <kcharles1972@gmail.com>
2023-02-05 10:03:09 -0700

    add NpIcon component to render np svgs

---
+++ src/lib/NpIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+  export let classNames = '';
+</script>
+
+{#await import(`./icons/${name}.svg?raw`) then Icon}
+  <span class="np-icon-w {classNames}">
+    {@html Icon.default}
+  </span>
+{/await}
+
+<style>
+  .np-icon-w {
+    display: inline-block;
+    width: 1.2em;
+    height: 1.2em;
+    vertical-align: middle;
+    transform: translateY(-0.1em);
+    fill: currentColor;
+  }
+</style>
